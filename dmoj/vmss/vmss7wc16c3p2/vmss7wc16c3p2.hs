a33dae0d962597fe2d2bcc1b5ee0f78b6db17dfb
Derek Xu <xuderekq@hotmail.com>
2022-03-08 12:07:23 -0500

    Moved vmss7wc16c3p2 solutions

--- dmoj/vmss/vmss7wc16c3p2/vmss7wc16c3p2.hs
+++ dmoj/vmss/vmss7wc16c3p2/vmss7wc16c3p2.hs
@@ -2,13 +2,13 @@ import Data.Array
 
 type AdjacencyList = Array Int [Int]
 
-buildGraph :: Int -> Int -> Int -> AdjacencyList -> IO AdjacencyList
-buildGraph 0 _ _ graph = return graph
-buildGraph i a b graph =
+buildGraph :: Int -> AdjacencyList -> IO AdjacencyList
+buildGraph 0 graph = return graph
+buildGraph i graph =
   getLine >>= \s ->
     let [u, v] = (map read . words) s
      in let updatedGraph = graph // [(u, v : (graph ! u)), (v, u : (graph ! v))]
-         in buildGraph (i - 1) a b updatedGraph
+         in buildGraph (i - 1) updatedGraph
 
 dfs :: AdjacencyList -> Int -> Int -> Array Int Bool -> Bool
 dfs graph u goal visited =
@@ -27,7 +27,7 @@ main :: IO ()
 main =
   getLine >>= \line ->
     let [n, m, a, b] = (map read . words) line
-     in buildGraph m a b (array (1, n) [(i, []) | i <- [1 .. n]])
+     in buildGraph m (array (1, n) [(i, []) | i <- [1 .. n]])
           >>= \graph ->
             if dfs graph a b (array (1, n) [(i, False) | i <- [1 .. n]])
               then putStrLn "GO SHAHIR!"
