98bf9d15c9f90dcde865e9b481bc0e200c06f593
Derek Xu <xuderekq@hotmail.com>
2022-03-14 09:12:18 -0400

    Improved the documentation of MinOrMaxQueue

--- Templates/MinOrMaxQueue.cpp
+++ Templates/MinOrMaxQueue.cpp
@@ -1,6 +1,4 @@
-#include <algorithm>
 #include <deque>
-#include <iostream>
 #include <utility>
 
 template <typename T>
@@ -11,11 +9,16 @@ class MinOrMaxQueue {
    private:
     Type type;
 
+    // A queue that holds pairs of elements and their effective indices. An invariant of `q` is that its first element
+    // is always the min/max (depending on `type`) element in the queue.
     std::deque<std::pair<T, int>> q;
 
+    // These variables are used to ensure that the interface of this data structure (push/pop) correctly behaves like
+    // that of a queue's.
     int insert_index = 0;
     int erase_index = 0;
 
+    // Helps ensure that `q`'s invariant always holds.
     bool shouldPopWhenInserting(const T& to_insert) {
         switch (type) {
             case Type::MIN:
@@ -28,6 +31,9 @@ class MinOrMaxQueue {
    public:
     MinOrMaxQueue(Type type) : type(type) {}
 
+    /**
+     * Pushes an element to the back of the queue.
+     */
     void pushBack(T to_insert) {
         while (!q.empty() && shouldPopWhenInserting(to_insert)) {
             q.pop_back();
@@ -36,6 +42,9 @@ class MinOrMaxQueue {
         insert_index++;
     }
 
+    /**
+     * Removes an element from the front of the queue.
+     */
     void popFront() {
         if (q.front().second == erase_index) {
             q.pop_front();
@@ -43,32 +52,8 @@ class MinOrMaxQueue {
         erase_index++;
     }
 
+    /**
+     * Returns the min/max (depending on the type of queue) element in the queue.
+     */
     T front() { return q.front().first; }
 };
-
-using namespace std;
-
-int main() {
-    MinOrMaxQueue<long long> q(MinOrMaxQueue<long long>::Type::MIN);
-
-    int n, a, b;
-    scanf("%i %i %i", &n, &a, &b);
-
-    long long sum[200001];
-    sum[0] = 0;
-    for (int i = 1; i <= n; i++) {
-        scanf("%lli", sum + i);
-        sum[i] += sum[i - 1];
-    }
-
-    long long ans = -9e18;
-    for (int i = a; i <= n; i++) {
-        if (i > b) {
-            q.popFront();
-        }
-        q.pushBack(sum[i - a]);
-        ans = max(ans, (long long)(sum[i] - q.front()));
-    }
-
-    printf("%lli\n", ans);
-}
