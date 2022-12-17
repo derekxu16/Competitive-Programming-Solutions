7121ca9ea5c072a6406619cdba22930b79318716
Derek Xu <xuderekq@hotmail.com>
2022-12-17 17:49:25 -0500

    Add day 6

---
+++ Advent of Code/2022/day 6/day6p2.hs
@@ -0,0 +1,18 @@
+import qualified Data.Set (fromList, size)
+
+solve :: String -> Int -> Int
+solve s answer =
+  let prefix = take 14 s
+   in if Data.Set.size (Data.Set.fromList prefix) == 14
+        then answer
+        else
+          let head : rest = s
+           in solve rest (answer + 1)
+
+main :: IO ()
+main =
+  interact
+    ( \full_input ->
+        show (solve full_input 14)
+          ++ "\n"
+    )
